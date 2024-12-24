d67e9179a799cba57dfecb8b0b0279f96c8464b9
taketheking <so4524@gmail.com>
2024-12-24 21:25:44 +0900

    🔧Add : Card 비지니스 3-layers 생성

---
+++ src/main/java/com/currency/teamflow/domain/card/controller/CardController.java
@@ -0,0 +1,9 @@
+package com.currency.teamflow.domain.card.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class CardController {
+}
---
+++ src/main/java/com/currency/teamflow/domain/card/service/CardService.java
@@ -0,0 +1,7 @@
+package com.currency.teamflow.domain.card.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CardService {
+}
