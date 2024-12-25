60d8685ac44b6599bdbcb40429f26dbf311a4566
j-hann <86395588+j-hann@users.noreply.github.com>
2024-12-26 03:22:54 +0900

    ✨ Feat : board create 기능 구현
    board 생성 기능 구현

---
+++ src/main/java/com/currency/teamflow/domain/board/dto/BoardResponseDto.java
@@ -0,0 +1,42 @@
+package com.currency.teamflow.domain.board.dto;
+
+import com.currency.teamflow.domain.board.entity.Board;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class BoardResponseDto {
+
+	private final Long id;//보드 id
+
+	private final String boardTitle;//보드 제목
+
+	private final String boardBackgroundColor;//보드 배경색
+
+	private final String imageUrl;//보드 이미지
+
+	private final LocalDateTime createdAt;//생성일
+
+	private final LocalDateTime modifiedAt;//수정일
+
+	public BoardResponseDto(Long id, String boardTitle, String boardBackgroundColor,
+		String imageUrl, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+		this.id = id;
+		this.boardTitle = boardTitle;
+		this.boardBackgroundColor = boardBackgroundColor;
+		this.imageUrl = imageUrl;
+		this.createdAt = createdAt;
+		this.modifiedAt = modifiedAt;
+	}
+
+	public static BoardResponseDto toDto(Board board, String imageUrl) {
+		return new BoardResponseDto(
+			board.getId(),
+			board.getBoardTitle(),
+			board.getBoardBackgroundColor(),
+			imageUrl,
+			board.getCreatedAt(),
+			board.getModifiedAt()
+		);
+	}
+}
---
+++ src/main/java/com/currency/teamflow/domain/board/repository/BoardRepository.java
@@ -0,0 +1,12 @@
+package com.currency.teamflow.domain.board.repository;
+
+
+import com.currency.teamflow.domain.board.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+
+}
