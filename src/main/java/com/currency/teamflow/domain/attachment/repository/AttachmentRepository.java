a5ad65854aa7887ad8ba312ff6838cc9cd436c6a
taketheking <so4524@gmail.com>
2024-12-26 23:25:32 +0900

    ✨ Feat : Attachment create API 기능 구현

---
+++ src/main/java/com/currency/teamflow/domain/attachment/repository/AttachmentRepository.java
@@ -0,0 +1,9 @@
+package com.currency.teamflow.domain.attachment.repository;
+
+import com.currency.teamflow.domain.attachment.entity.Attachment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
+}
