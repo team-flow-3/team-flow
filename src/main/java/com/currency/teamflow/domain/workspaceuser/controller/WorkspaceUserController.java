3ca46f7c3ec7e07aa43a5e7410f0859c4a3adb98
myoma <102959354+GyeonghwanCheon@users.noreply.github.com>
2024-12-26 22:21:32 +0900

    ✨ Feat : 워크스페이스 관리자 권한 삭제 기능 구현
    * ✨ Feat : 워크스페이스 관리자 권한 삭제 기능 구현

--- src/main/java/com/currency/teamflow/domain/workspaceuser/controller/WorkspaceUserController.java
+++ src/main/java/com/currency/teamflow/domain/workspaceuser/controller/WorkspaceUserController.java
@@ -33,5 +33,19 @@ public class WorkspaceUserController {
 
         return ResponseEntity.ok(responseDto);
     }
+
+    /**
+     * ADMIN이 특정 워크스페이스 관리자를 삭제
+     * @param workspaceUserId 워크스페이스 사용자 ID
+     * @return 삭제 완료 메시지
+     */
+    @CheckUserRole(requiredAuthorities = {Auth.ADMIN}) // ADMIN만 접근 가능
+    @DeleteMapping("/admin/workspaceUser/{workspaceUserId}")
+    public ResponseEntity<String> deleteWorkspaceAdmin(@PathVariable Long workspaceUserId) {
+
+        workspaceUserService.deleteWorkspaceAdmin(workspaceUserId);
+
+        return ResponseEntity.ok("워크스페이스 관리자 삭제 완료");
+    }
 }
 
