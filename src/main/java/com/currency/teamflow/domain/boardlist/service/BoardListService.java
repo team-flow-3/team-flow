5a0037ff86323c92d3c3863819351c4209e53b37
j-hann <86395588+j-hann@users.noreply.github.com>
2024-12-29 00:49:35 +0900

    ✨ Feat : 보드 리스트 삭제 API 기능 구현
    ✨ Feat : 보드 리스트 삭제 API 기능 구현
    
    🔧 Add : 보드 생성 API 기능 수정
    - 로그인한 유저가 보드 리스트 생성할 수 있는 워크스페이스 초대와 권한이 있는지 확인

--- src/main/java/com/currency/teamflow/domain/boardlist/service/BoardListService.java
+++ src/main/java/com/currency/teamflow/domain/boardlist/service/BoardListService.java
@@ -8,9 +8,10 @@ import com.currency.teamflow.domain.boardlist.entity.BoardList;
 import com.currency.teamflow.domain.boardlist.repository.BoardListRepository;
 import com.currency.teamflow.domain.user.entity.User;
 import com.currency.teamflow.domain.user.entity.WorkspaceUser;
+import com.currency.teamflow.domain.workspace.repository.WorkspaceRepository;
 import com.currency.teamflow.domain.workspaceuser.repository.WorkspaceUserRepository;
 import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -21,31 +22,38 @@ public class BoardListService {
 	private final BoardListRepository boardListRepository;
 	private final BoardRepository boardRepository;
 	private final WorkspaceUserRepository workspaceUserRepository;
+	private final WorkspaceRepository workspaceRepository;
 
 	public BoardListService(BoardListRepository boardListRepository,
-		BoardRepository boardRepository, WorkspaceUserRepository workspaceUserRepository) {
+		BoardRepository boardRepository, WorkspaceUserRepository workspaceUserRepository,
+		WorkspaceRepository workspaceRepository) {
 		this.boardListRepository = boardListRepository;
 		this.boardRepository = boardRepository;
 		this.workspaceUserRepository = workspaceUserRepository;
+		this.workspaceRepository = workspaceRepository;
 	}
 
 	/**
 	 * 보드 리스트 생성 API
 	 * - 워크스페이스 관리자, 보드 권한 가진 유저 허용
+	 * TODO : 로그 정리
 	 */
 	@Transactional
 	public BoardListResponseDto createBoardList(BoardListRequestDto boardListRequestDto,
 		User loginedUser) {
+		//로그 확인
 		log.info("logindeUser : {}" , loginedUser.getId());
-		//로그인한 유저가 자신이 속한 워크스페이스의 보드 테이블을 만들 수 있는 유저인지 확인 -> 워크스페이스 아이디 가져오기
-		WorkspaceUser findWorkspace = workspaceUserRepository.findWorkspaceIdByUserIdOrElseThrow(loginedUser.getId());
-		log.info("findWorkspace : {}" , findWorkspace);
+		//로그인한 유저 = 초대받은 워크스페이스 보드 리스트를 생성할 수 있는 유저인지 확인
+		//-> 워크스페이스 id 가져오기
+		Optional<WorkspaceUser> workspace = workspaceUserRepository.findWorkspaceIdByUserIdOrElseThrow(loginedUser.getId());
+		log.info("workspaceId : {}" , workspace);
+
 		//보드 id 가져오기
-		Board board = boardRepository.findByIdOrElseThrow(findWorkspace.getId());
-		log.info("board : {}" , board);
+		Board board = boardRepository.findByIdOrElseThrow(boardListRequestDto.getBoardId());
+		log.info("boardId : {}" , board.getId());
+
 		//리스트 크기, array 최대값
 		int arrayNumber = board.getBoardLists().size();
-
 		//리스트 생성
 		BoardList boardList = new BoardList(board, boardListRequestDto.getBoardListTitle());
 		boardList.addArrayNumber(arrayNumber + 1);
@@ -66,4 +74,14 @@ public class BoardListService {
 		return BoardListResponseDto.toDto(boardList);
 	}
 
+	/**
+	 * 보드 리스트 삭제 API
+	 * - 워크스페이스 관리자, 보드 권한 보유 유저 허용
+	 * - 삭제시 모든 카드와 데이터도 삭제
+	 */
+	public void deleteBoardList(Long boardListId) {
+		//보드 리스트 정보 가져오기
+		BoardList boardList = boardListRepository.findByIdOrElseThrow(boardListId);
+		boardListRepository.delete(boardList);
+	}
 }
