658279ef3238a724b79468430660ff606217da2f
j-hann <j.han77055@gmail.com>
2024-12-29 17:29:04 +0900

    ✨ Feat : 보드 수정 API 기능 구현
    보드 생성 API 기능 수정

--- src/main/java/com/currency/teamflow/domain/boardlist/service/BoardListService.java
+++ src/main/java/com/currency/teamflow/domain/boardlist/service/BoardListService.java
@@ -6,12 +6,12 @@ import com.currency.teamflow.domain.boardlist.dto.BoardListRequestDto;
 import com.currency.teamflow.domain.boardlist.dto.BoardListResponseDto;
 import com.currency.teamflow.domain.boardlist.entity.BoardList;
 import com.currency.teamflow.domain.boardlist.repository.BoardListRepository;
-import com.currency.teamflow.domain.user.entity.User;
 import com.currency.teamflow.domain.user.entity.WorkspaceUser;
 import com.currency.teamflow.domain.workspace.repository.WorkspaceRepository;
 import com.currency.teamflow.domain.workspaceuser.repository.WorkspaceUserRepository;
+import com.currency.teamflow.global.error.errorcode.ErrorCode;
+import com.currency.teamflow.global.error.exception.CustomException;
 import jakarta.transaction.Transactional;
-import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -34,22 +34,20 @@ public class BoardListService {
 	}
 
 	/**
-	 * 보드 리스트 생성 API
+	 * 보드 리스트 생성 서비스 메서드
 	 * - 워크스페이스 관리자, 보드 권한 가진 유저 허용
 	 * TODO : 로그 정리
 	 */
 	@Transactional
 	public BoardListResponseDto createBoardList(BoardListRequestDto boardListRequestDto,
-		User loginedUser) {
-		//로그 확인
-		log.info("logindeUser : {}" , loginedUser.getId());
-		//로그인한 유저 = 초대받은 워크스페이스 보드 리스트를 생성할 수 있는 유저인지 확인
-		//-> 워크스페이스 id 가져오기
-		Optional<WorkspaceUser> workspace = workspaceUserRepository.findWorkspaceIdByUserIdOrElseThrow(loginedUser.getId());
-		log.info("workspaceId : {}" , workspace);
+		WorkspaceUser workspaceUser) {
 
-		//보드 id 가져오기
-		Board board = boardRepository.findByIdOrElseThrow(boardListRequestDto.getBoardId());
+		//보드 정보 가져오기
+		Board board = boardRepository.findBoardIdByWorkspaceId(workspaceUser.getWorkspace().getId(), boardListRequestDto.getBoardId())
+			.orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_BOARD));
+
+//		//보드 정보 가져오기
+//		Board board = boardRepository.findByIdOrElseThrow(boardListRequestDto.getBoardId());
 		log.info("boardId : {}" , board.getId());
 
 		//리스트 크기, array 최대값
@@ -64,7 +62,7 @@ public class BoardListService {
 	}
 
 	/**
-	 * 보드 리스트 단건 조회 API
+	 * 보드 리스트 단건 조회 서비스 메서드
 	 * - 워크스페이스 초대된 모든 유저 가능
 	 */
 	public BoardListResponseDto selectBoardList(Long boardListId) {
@@ -75,7 +73,7 @@ public class BoardListService {
 	}
 
 	/**
-	 * 보드 리스트 삭제 API
+	 * 보드 리스트 삭제 서비스 메서드
 	 * - 워크스페이스 관리자, 보드 권한 보유 유저 허용
 	 * - 삭제시 모든 카드와 데이터도 삭제
 	 */
