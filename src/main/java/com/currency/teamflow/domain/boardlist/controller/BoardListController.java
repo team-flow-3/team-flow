7f6c91d1c57bd26bb5aaee0519c3d65054d39675
j-hann <86395588+j-hann@users.noreply.github.com>
2024-12-30 17:18:58 +0900

    ✨ Feat : 보드 리스트 수정 API 기능 구현
    ✨ Feat : 보드 리스트 수정 API 기능 구현
    - 보드 리스트 제목 수정 기능
    - 보드 리스트 순서 변경 기능

--- src/main/java/com/currency/teamflow/domain/boardlist/controller/BoardListController.java
+++ src/main/java/com/currency/teamflow/domain/boardlist/controller/BoardListController.java
@@ -2,6 +2,7 @@ package com.currency.teamflow.domain.boardlist.controller;
 
 import com.currency.teamflow.domain.boardlist.dto.BoardListRequestDto;
 import com.currency.teamflow.domain.boardlist.dto.BoardListResponseDto;
+import com.currency.teamflow.domain.boardlist.dto.BoardListUpdateRequestDto;
 import com.currency.teamflow.domain.boardlist.service.BoardListService;
 import com.currency.teamflow.domain.user.entity.WorkspaceUser;
 import com.currency.teamflow.global.annotation.CheckMemberRole;
@@ -14,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -77,4 +79,29 @@ public class BoardListController {
 
 		return new ResponseEntity<>("보드 리스트가 삭제되었습니다.", HttpStatus.OK);
 	}
+
+	/**
+	 * 보드 리스트 수정 API
+	 * - 워크스페이스 관리자, 보드 권한 보유 유저 허용
+	 * - 보드 내에서의 순서를 변경할 수 있다
+	 */
+	@CheckMemberRole(requiredRoles = {Role.WORKSPACE_ADMIN , Role.BOARD_USER})
+	@PatchMapping("/{boardListId}")
+	public ResponseEntity<BoardListResponseDto> updateBoardList(
+		@PathVariable Long boardListId,
+		@RequestBody BoardListUpdateRequestDto boardListUpdateRequestDto,
+		HttpServletRequest httpServletRequest){
+
+		HttpSession httpSession = httpServletRequest.getSession(false);
+		WorkspaceUser workspaceUser = (WorkspaceUser) httpSession.getAttribute("workspaceUser");
+
+		BoardListResponseDto boardListResponseDto = boardListService.updateBoardList(
+			boardListId,
+			workspaceUser,
+			boardListUpdateRequestDto
+		);
+
+		return new ResponseEntity<>(boardListResponseDto, HttpStatus.OK);
+	}
+	
 }
